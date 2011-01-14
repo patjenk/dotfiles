01fea87bb2969b38ad7029e32774c4b011b33a56
Patrick Jenkins <patjenk@gmail.com>
2011-01-14 04:55:59 +0000

    Stealing memcache-top from rtomayko's dotfiles and adding an ipython config file to automatically load my django models.

---
+++ ipython/ipy_user_conf.py
@@ -0,0 +1,20 @@
+import IPython.ipapi
+ip = IPython.ipapi.get()
+import os
+
+def load_django_models():
+    """Load all of our django models. Taken from django snippsets. 
+    http://djangosnippets.org/snippets/549/"""
+    try:
+        from django.db.models.loading import get_models
+        for m in get_models():
+            ip.ex("from %s import %s" % (m.__module__, m.__name__))
+        print 'INFO: Loaded Django models.'
+    except ImportError as e:
+        print e
+        pass
+
+def main():
+    load_django_models()
+
+main()
