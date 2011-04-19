61a80350f3a90ea7d37715211ffe34838d67d9cd
Patrick Jenkins <patjenk@gmail.com>
2011-04-19 01:05:28 +0000

    load the django Q queryset object by default in ipython.

--- ipython/ipy_user_conf.py
+++ ipython/ipy_user_conf.py
@@ -6,6 +6,7 @@ def load_django_models():
     """Load all of our django models. Taken from django snippsets. 
     http://djangosnippets.org/snippets/549/"""
     try:
+        from django.db.models import Q
         from django.db.models.loading import get_models
         for m in get_models():
             ip.ex("from %s import %s" % (m.__module__, m.__name__))
