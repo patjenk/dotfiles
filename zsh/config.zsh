a558003cb440828aceae20f04d31e0a8077ab267
Patrick Jenkins <patjenk@ibis.(none)>
2012-10-18 17:25:10 -0500

    For zsh shell, ctrl-e and ctrl-a go to the beginning and end of lines respectively when in vim modes. This is emacs like but im used to it.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -46,10 +46,13 @@ zle -N newtab
 
 bindkey -v            # use vim mode. YOU SHOULD BE IN AWE RIGHT NOW!
 
+bindkey -M viins '^A' beginning-of-line
+bindkey -M viins '^E' end-of-line
+bindkey -M vicmd '^A' beginning-of-line
+bindkey -M vicmd '^E' end-of-line
+
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
