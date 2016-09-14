dd50a5e6e703a731c902ce673fa525dc3752ec6b
Patrick Jenkins <patrick@socialgrowthtechnologies.com>
2016-09-14 15:38:10 -0400

    making the install.py script python3 safe (or at least safer).

--- install.py
+++ install.py
@@ -19,6 +19,10 @@ parser.add_argument("-f", "--force",
                     help="force the installation of these dotfiles. overwrite all current dotfiles blindly.")
 args = parser.parse_args()
 
+try:
+    input = raw_input
+except:
+    pass
 
 def install(pretend, force):
     """
@@ -43,49 +47,49 @@ def install(pretend, force):
         if os.path.isfile(full_filename) :
             if os.path.isfile(dot_filename) or os.path.islink(dot_filename):
                 if (os.path.islink(dot_filename) and os.path.samefile(filename, os.readlink(dot_filename))) or (os.path.isfile(dot_filename) and filecmp.cmp(full_filename, dot_filename)):
-                    print "identical file: {filename} and {dot_filename}".format(filename=full_filename, dot_filename=dot_filename)
+                    print("identical file: {filename} and {dot_filename}".format(filename=full_filename, dot_filename=dot_filename))
                 else:
                     overwrite_choice = ""
                     while overwrite_choice not in ["y", "n"]:
                         if not force:
-                            overwrite_choice = raw_input("overwrite {filename} [y/n]? ".format(filename=dot_filename))
+                            overwrite_choice = input("overwrite {filename} [y/n]? ".format(filename=dot_filename))
                         else:
                             overwrite_choice = "y"
                         if overwrite_choice == "y":
                             if not pretend:
                                 os.remove(dot_filename)
                                 os.symlink(full_filename, dot_filename)
-                            print "replaced file: {dot_filename}".format(dot_filename=dot_filename)
+                            print("replaced file: {dot_filename}".format(dot_filename=dot_filename))
                         elif overwrite_choice == "n":
-                            print "skipped file: {dot_filename}".format(dot_filename=dot_filename)
+                            print("skipped file: {dot_filename}".format(dot_filename=dot_filename))
             else:
                 # the file doesn't exist, create it!
                 if not pretend:
                     os.symlink(full_filename, dot_filename)
-                print "linked file: {dot_filename}".format(dot_filename=dot_filename)
+                print("linked file: {dot_filename}".format(dot_filename=dot_filename))
         elif os.path.isdir(full_filename):
             if os.path.isdir(dot_filename) or os.path.islink(dot_filename):
                 if os.path.islink(dot_filename) and os.readlink(dot_filename) == full_filename:
-                    print "identical directory: {filename} and {dot_filename}".format(filename=filename, dot_filename=dot_filename)
+                    print("identical directory: {filename} and {dot_filename}".format(filename=filename, dot_filename=dot_filename))
                 else:
                     overwrite_choice = ""
                     while overwrite_choice not in ["y", "n"]:
                         if not force:
-                            overwrite_choice = raw_input("overwrite {filename} [y/n]? ".format(filename=dot_filename))
+                            overwrite_choice = input("overwrite {filename} [y/n]? ".format(filename=dot_filename))
                         else:
                             overwrite_choice = "y"
                         if overwrite_choice == "y":
                             if not pretend:
                                 shutil.rmtree(dot_filename)
                                 os.symlink(full_filename, dot_filename)
-                            print "replaced directory: {dot_filename}".format(dot_filename=dot_filename)
+                            print("replaced directory: {dot_filename}".format(dot_filename=dot_filename))
                         elif overwrite_choice == "n":
-                            print "skipped directory: {dot_filename}".format(dot_filename=dot_filename)
+                            print("skipped directory: {dot_filename}".format(dot_filename=dot_filename))
             else:
                 # the file doesn't exist, create it!
                 if not pretend:
                     os.symlink(full_filename, dot_filename)
-                print "linked directory: {dot_filename}".format(dot_filename=dot_filename)
+                print("linked directory: {dot_filename}".format(dot_filename=dot_filename))
         else:
             raise Exception("Unknown file {filename=filename}".format(filename=full_filename))
     pass
