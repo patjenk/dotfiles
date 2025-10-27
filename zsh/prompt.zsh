4259c6e3cdbbb8e851de5dc157068901f294c38b
Patrick Jenkins <me@patjenk.com>
2025-10-27 16:15:24 -0400

    Updating the zsh prompt to add the hostname.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,22 +1,23 @@
-# Minimal, fast prompt with short path and git branch on the right.
 autoload -Uz colors vcs_info && colors
-
-# Shorten %~ by trimming to last 2 path components
 typeset -g PROMPT_DIRTRIM=2
 
-# ----- vcs_info (git branch) -----
+# vcs_info for git branch on right
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git*' formats 'git:%b'
 zstyle ':vcs_info:git*' actionformats 'git:%b|%a'
 
-precmd() { vcs_info }   # populates ${vcs_info_msg_0_}
+precmd() { vcs_info }
 
-# Left: short path + prompt char
-PROMPT='%F{cyan}%~%f %# '
+# Build prompt depending on connection type
+if [[ -n $SSH_CONNECTION ]]; then
+  # Remote session: show user@host
+  PROMPT='%F{yellow}[%n@%m]%f %F{cyan}%~%f %# '
+else
+  # Local session: show only host
+  PROMPT='%F{yellow}[%m]%f %F{cyan}%~%f %# '
+fi
 
-# Right: non-zero exit status (if any) + git branch
+# Right prompt (git branch and exit status)
 RIGHT_STATUS='$(ret=$?; ((ret)) && print -r -- "exit:$ret")'
 RPROMPT='%F{8}'"$RIGHT_STATUS"'%f %F{magenta}${vcs_info_msg_0_}%f'
 
-# Nerd Font option:
-# zstyle ':vcs_info:git*' formats ' %b'
